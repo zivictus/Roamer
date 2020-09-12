259fe5f549f488b54a7de785e73a4ca541fd859b
Dmitry <cookiecatisbestgmail.com>
2020-09-12 15:32:41 +0300

    Add game itself

---
+++ Roamer/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxes Axes;
+    public float Sensitivity;
+    public float MaxVertical;
+    public float MinVertical;
+
+    private float _rotationX;
+    private float _rotationY;
+
+    MouseLook()
+    {
+        _rotationX = 0;
+        _rotationY = 0;
+
+        Axes = RotationAxes.MouseX;
+        Sensitivity = 5.0f;
+        MaxVertical = 45f;
+        MinVertical = -MaxVertical;
+    }
+
+    void Update()
+    {
+        if (Axes == RotationAxes.MouseX)
+        {
+            _rotationY += Input.GetAxis("Mouse X") * Sensitivity;
+            transform.localEulerAngles = new Vector3(0, _rotationY, 0);
+        }
+        else if (Axes == RotationAxes.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * Sensitivity;
+            _rotationX = Mathf.Clamp(_rotationX, MinVertical, MaxVertical);
+
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        else
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * Sensitivity;
+            _rotationX = Mathf.Clamp(_rotationX, MinVertical, MaxVertical);
+
+            _rotationY += Input.GetAxis("Mouse X") * Sensitivity;
+            transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
+        }
+    }
+}
---
+++ Roamer/Assets/Scripts/Spin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float RotationSpeed;
+
+    Spin()
+    {
+        RotationSpeed = 1.0f;
+    }
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotationSpeed, RotationSpeed, RotationSpeed);
+    }
+}
