b96577c0d208c98ae1941350b579ee93a556910e
Dmitry <cookiecatisbestgmail.com>
2020-09-13 19:49:07 +0300

    Add trees and shooting script

--- Roamer/Assets/Scripts/FPSInput.cs
+++ Roamer/Assets/Scripts/FPSInput.cs
@@ -8,6 +8,11 @@ public class FPSInput : MonoBehaviour
 {
     public float speed = 6.0f;
     public float gravity = -9.8f;
+    public float maxCornerLook = 3;
+    public float cornerLookSpeed = 1;
+    public float jumpHeight = 10;
+    public float shiftSpeed = 2;
+
     private CharacterController _charController;
     void Start()
     {
@@ -17,11 +22,26 @@ public class FPSInput : MonoBehaviour
     {
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
+
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        Vector3 jump = new Vector3(0, jumpHeight, 0);
+
         movement = Vector3.ClampMagnitude(movement, speed);
         movement.y = gravity;
         movement *= Time.deltaTime;
+
+        jump = transform.TransformDirection(jump);
         movement = transform.TransformDirection(movement);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movement.x = movement.x * shiftSpeed;
+            movement.z = movement.z * shiftSpeed;
+        }
+
         _charController.Move(movement);
+
+        if (Input.GetButtonDown("Jump"))
+            _charController.Move(jump);
     }
 }
--- Roamer/Assets/Scripts/MouseLook.cs
+++ Roamer/Assets/Scripts/MouseLook.cs
@@ -16,18 +16,36 @@ public class MouseLook : MonoBehaviour
     public float MaxVertical;
     public float MinVertical;
 
+    public float MaxZRotation;
+    public float MinZRotation;
+
     private float _rotationX;
     private float _rotationY;
+    private float _rotationRightZ;
+    private float _rotationLeftZ;
+    private float _rotationZ;
+
+    private float cornerLook;
+    private float lookAroundLeft;
+    private float lookAroundRight;
+    private Vector3 lookAround;
 
     MouseLook()
     {
         _rotationX = 0;
         _rotationY = 0;
+        _rotationRightZ = 0;
+        _rotationLeftZ = 0;
+        _rotationLeftZ = 0;
+
+        cornerLook = 2f;
 
         Axes = RotationAxes.MouseX;
         Sensitivity = 5.0f;
         MaxVertical = 45f;
         MinVertical = -MaxVertical;
+        MaxZRotation = 15;
+        MinZRotation = 0;
     }
 
     void Update()
@@ -43,7 +61,9 @@ public class MouseLook : MonoBehaviour
             _rotationX = Mathf.Clamp(_rotationX, MinVertical, MaxVertical);
 
             float rotationY = transform.localEulerAngles.y;
-            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, LookAroundCorner());
+            transform.localPosition = new Vector3(-LookAroundCorner()*0.1f, 0);
         }
         else
         {
@@ -51,7 +71,20 @@ public class MouseLook : MonoBehaviour
             _rotationX = Mathf.Clamp(_rotationX, MinVertical, MaxVertical);
 
             _rotationY += Input.GetAxis("Mouse X") * Sensitivity;
-            transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
+
+            transform.localEulerAngles = new Vector3(_rotationX, _rotationY, _rotationZ);
+            transform.localPosition = new Vector3(0, 0, _rotationZ);
         }
+    }
+
+    public float LookAroundCorner()
+    {
+        _rotationRightZ += Input.GetKey(KeyCode.Q) ? cornerLook : -cornerLook;
+        _rotationRightZ = Mathf.Clamp(_rotationRightZ, MinZRotation, MaxZRotation);
+
+        _rotationLeftZ += Input.GetKey(KeyCode.E) ? -cornerLook : cornerLook;
+        _rotationLeftZ = Mathf.Clamp(_rotationLeftZ, -MaxZRotation, MinZRotation);
+
+        return _rotationLeftZ + _rotationRightZ;
     }
 }
